3a9acf88d49cadaa5c4b3b502a655abea98093ae
Shem <sammyshem15@gmail.com>
2025-12-16 17:35:55 +0300

    Update and rename week 2.c to volume and surface area.c

--- volume and surface area.c
+++ volume and surface area.c
@@ -1,14 +1,20 @@
+/*
+Name: Shem Sammy
+Reg No: PA106/G/28607/25
+Description: A program to find the surface area and volume of a cylinder 
+*/
 #include <stdio.h>
 
 int main(){
+    //prompt to enter radius
     printf("Input radius;");
     float r;
     scanf("%f",&r);
-    
+    //prompt to enter height
     printf("Input height;");
     float h;
     scanf("%f",&h);
-    
+    //execution
     float volume=3.14159*r*r*h;
     
     printf("The volume of the cylinder if %.2f\n",volume);
